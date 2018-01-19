0ba227aec4110e2a6e30631e2452e46fa35d95f8
Lisandro Jofré <lisandrojofre@gmail.com>
2018-01-19 11:26:31 -0300

    Update do-a-barrel-roll.js

--- dist/do-a-barrel-roll.js
+++ dist/do-a-barrel-roll.js
@@ -13,15 +13,23 @@
     styles.innerHTML = "@-moz-keyframes roll{100%{-moz-transform:rotate(360deg)}}@-o-keyframes roll{100%{-o-transform:rotate(360deg)}}@-webkit-keyframes roll{100%{-webkit-transform:rotate(360deg)}}.do-a-barrel-roll{-moz-animation-duration:2s;-moz-animation-name:roll;-moz-animation-iteration-count:1;-webkit-animation-duration:2s;-webkit-animation-name:roll;-webkit-animation-iteration-count:1;-o-animation-duration:2s;-o-animation-name:roll;-o-animation-iteration-count:1}";
     document.getElementsByTagName("head")[0].appendChild(styles);
 
+    var interval;
     var keyed = "";
+
     document.addEventListener("keypress", function(event) {
-        keyed += String.fromCharCode(event.charCode);
-        if (event.charCode === 13 && keyed.toLowerCase().indexOf("doabarrelroll") !== -1) {
+        if (event.charCode === 13 && keyed.toLowerCase().substr(-13) === "doabarrelroll") {
+            keyed = "";
             document.body.classList.add("do-a-barrel-roll");
             setTimeout(function() {
                 document.body.classList.remove("do-a-barrel-roll");
-                keyed = "";
             }, 2000);
         }
+
+        clearInterval(interval);
+        interval = setInterval(function(){
+            keyed = "";
+        }, 5000);
+        
+        keyed += String.fromCharCode(event.charCode);
     });
 })();
